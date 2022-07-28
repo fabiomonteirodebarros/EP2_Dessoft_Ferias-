9ff53192acebd71df07c72ccee15eda51ba46d31
Fabio Monteiro de Barros Neto <fabiombn@al.insper.edu.br>
2022-07-28 13:52:13 -0300

    continuacao do jogo

--- Jogo.py
+++ Jogo.py
@@ -1,3 +1,15 @@
+from Funcoes import *
+from Questoes import *
+dic_vazio=[]
+problema=valida_questoes(dic)
+if len(problema)!= problema.count(dic_vazio):
+    print('A base está errada')
+    quit()
+
+transforma= transforma_base(dic)
+
+
+
 nome = input('Digite o seu nome: ')
 
 print ('Ok, {0}'.format(nome))
@@ -11,7 +23,10 @@ print('BOM JOGO!')
 
 print('O jogo já vai começar!')
 sim= input('Digite "sim" se deseja começar o jogo')
+acerto= True
 
 if sim == 'sim':
-    sim+=1
+    while acerto:
+       i+=1
+
 
