b2022f0d4ff4d94be0475dc0f6663ff61e200045
Fabio Monteiro de Barros Neto <fabiombn@al.insper.edu.br>
2022-07-28 09:39:51 -0300

    Mais alguns prints

--- Jogo.py
+++ Jogo.py
@@ -4,5 +4,14 @@ print ('Ok, {0}'.format(nome))
 print('Segue abaixo um manual de regras:')
 print('- Você tem direito de realizar 3 pulos')
 print('- Você tem direito de pedir 2 ajudas')
+print('- As dificuldades das perguntas vão aumentando conforme o jogador acerta as respostas')
 print('- Os comandos possiveis são:\n- "A"\n-"B"\n-"C"\n-"D"\n-"ajuda"\n-"pula"\n-"parar"')
 print('BOM JOGO!')
+
+
+print('O jogo já vai começar!')
+sim= input('Digite "sim" se deseja começar o jogo')
+
+if sim == 'sim':
+    sim+=1
+
