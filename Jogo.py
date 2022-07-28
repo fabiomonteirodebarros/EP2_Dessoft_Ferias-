96f523be9dfc341b92139b1f2b57a099cd3e1200
Fabio Monteiro de Barros Neto <fabiombn@al.insper.edu.br>
2022-07-28 00:45:17 -0300

    prints iniciais

---
+++ Jogo.py
@@ -0,0 +1,8 @@
+nome = input('Digite o seu nome: ')
+
+print ('Ok, {0}'.format(nome))
+print('Segue abaixo um manual de regras:')
+print('- Você tem direito de realizar 3 pulos')
+print('- Você tem direito de pedir 2 ajudas')
+print('- Os comandos possiveis são:\n- "A"\n-"B"\n-"C"\n-"D"\n-"ajuda"\n-"pula"\n-"parar"')
+print('BOM JOGO!')
